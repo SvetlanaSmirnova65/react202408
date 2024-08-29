938ae2079119b0ee8349bbafdc26d8b4dc97cf6b
Svetlana S <sm.smirnova@severstal.com>
2024-08-29 15:19:57 +0300

    Форма отзыва

--- src/components/restaurant/restaurant.jsx
+++ src/components/restaurant/restaurant.jsx
@@ -1,5 +1,5 @@
-import { Counter } from "../counter/counter";
 import { Menu } from "../menu/menu";
+import { ReviewForm } from "../review-form/review-form";
 import { Reviews } from "../reviews/reviews";
 
 export const Restaurant = ({ name, menu, reviews }) => {
@@ -14,6 +14,8 @@ export const Restaurant = ({ name, menu, reviews }) => {
       {menu.length ? <Menu menu={menu} /> : <div>Нет меню</div>}  
       <h3>Отзывы</h3>
       {reviews.length ? <Reviews reviews={reviews} /> : <div>Нет отзывов</div>}
+      <h3>Создать отзыв</h3>
+      <ReviewForm/>
     </section>
   );
 };
