07495ddbf32dfeea1a401e18f118860a075139ed
Svetlana S <sm.smirnova@severstal.com>
2024-09-10 11:28:12 +0300

    hw-6 Исправлено

--- src/components/review/review.jsx
+++ src/components/review/review.jsx
@@ -6,12 +6,12 @@ export const Review = ({id }) => {
    const review = useSelector((state) => selectReviewById(state, id));
    const { userId, text, rating } = review || {};
  
+   const user = useSelector((state) => selectUserById(state, userId));
+   const { name } = user;
+
    if (!text) {
      return null;
    }
-
-   const user = useSelector((state) => selectUserById(state, userId));
-   const { name } = user;
 
    return (
        <>{name}: {text}  <span style={{ color: 'red' }}>{rating}</span></>
